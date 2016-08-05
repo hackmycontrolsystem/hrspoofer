4acbdc3b680d8040b41144c7a3531df0a385f9ec
AJ NOURI <ajn@localhost.localdomain>
2016-08-05 05:03:30 +0200

    Correct CSV file variable

--- 2.7/hrspoofer.py
+++ 2.7/hrspoofer.py
@@ -7,17 +7,19 @@ import sys
 import httplib
  
 total_args = len(sys.argv)
-if total_args != 4:
+if total_args != 5:
     print "\n"
-    print "Usage: ./%s <destination host> <destination port> <file.csv>" %sys.argv[0]
+    print "Usage: ./%s <destination host> <destination port> <requested_page> <file.csv>" %sys.argv[0]
     print "    destination host/port  : Web server virtual IP and port to test"
+    print "    requested page         : Keep empty if default index pages"
     print "    <file.csv>   : obtained using the script >>  gen_temp_ip.py << \n"
     sys.exit(1)
 
 IP_DST=sys.argv[1]
 PORT_DST=sys.argv[2]
-
-with open('ip-port.csv') as ip_port_csvfile:
+CSV_FILE=sys.argv[4]
+PAGE=sys.argv[3]
+with open(CSV_FILE) as ip_port_csvfile:
     reader = csv.reader(ip_port_csvfile, delimiter=',')
     for row in reader:
         IP_SRC=row[0]
@@ -25,10 +27,10 @@ with open('ip-port.csv') as ip_port_csvfile:
         print "Sending request from ( %s , %s )" %(IP_SRC,PORT_SRC)
         try:
 	  conn = httplib.HTTPConnection(host=IP_DST, port=PORT_DST, source_address=(IP_SRC,PORT_SRC),timeout=2)
-	  conn.request("GET", "/")
+	  conn.request("GET", PAGE)
 	  res = conn.getresponse()
-	  #print res.status, res.reason
+	  print res.status, res.reason
         
         except:
-          print "Unexpected error:", sys.exc_info()[0]      
+          print "Unexpected error:", sys.exc_info()[0]   
 
